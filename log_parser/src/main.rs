9a6b0ea0179e820352c7126a8f507370e8b1f118
VelSkorp <ukantsevich@gmail.com>
2024-10-08 19:34:09 +0200

    Added multi-threaded file processing

--- log_parser/src/main.rs
+++ log_parser/src/main.rs
@@ -1,5 +1,6 @@
 use std::fs::File;
 use std::io::{self, BufRead, Write};
+use rayon::prelude::*;
 use regex::Regex;
 
 fn main() -> io::Result<()> {
@@ -14,7 +15,6 @@ fn main() -> io::Result<()> {
         .expect("Failed to read path");
 
     let path = path.trim();
-
     let file = File::open(&path)?;
     let reader = io::BufReader::new(file);
 
@@ -29,20 +29,32 @@ fn main() -> io::Result<()> {
         .expect("Failed to read log level");
 
     let log_level = log_level.trim();
-
-    let re = Regex::new(log_level).unwrap();
-
+    let regex = Regex::new(log_level).unwrap();
+    let chunk_size = 1000;
+    let mut lines: Vec<String> = Vec::new();
     let mut count = 0;
 
     for line in reader.lines() {
         let line = line?;
-        if re.is_match(&line) {
-            count += 1;
-            println!("{}", line);
+        lines.push(line);
+
+        if lines.len() >= chunk_size {
+            let chunk = lines.split_off(0);
+            count += process_log_lines(chunk, &regex);
         }
+    }
+
+    if !lines.is_empty() {
+        count += process_log_lines(lines, &regex);
     }
 
     println!("Total number of {log_level}: {count}");
 
     Ok(())
+}
+
+fn process_log_lines(lines:Vec<String>, regex: &Regex) -> usize {
+    lines.into_par_iter()
+        .filter(|line| regex.is_match(line))
+        .count()
 }
